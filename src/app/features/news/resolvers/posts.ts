360b02d8406599bb769d67332d89a1d842b2646a
Damian Kowalski <dkowalski.1997@gmail.com>
2024-10-24 02:55:09 +0200

    wip

--- src/app/features/news/resolvers/posts.ts
+++ src/app/features/news/resolvers/posts.ts
@@ -2,7 +2,11 @@ import { ResolveFn } from "@angular/router";
 import { News } from "../types/news";
 import { inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { catchError, of } from "rxjs";
 
 export const newsResolver: ResolveFn<News[]> = () => {
-    return inject(HttpClient).get<News[]>('api/article/all');
+    return inject(HttpClient).get<News[]>('api/article/all').pipe(catchError((err) => {
+        console.log(err);
+        return of([])
+    }));
 }
