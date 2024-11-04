a3f37ef5e0da5b6f23c710b7ae729c2b331fd70b
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-05 00:38:06 +0100

    wip

--- apps/api/src/db/database.ts
+++ apps/api/src/db/database.ts
@@ -10,13 +10,18 @@ dotenv.config();
  */
 export const connectDatabase = (): Promise<void> => {
     console.log(`Connecting to the database. The uri: ${process.env['DB_URI']}`)
-    const uri = process.env['DB_URI'];
+    const uri = `${process.env['DB_URI']}/${process.env['DB_NAME']}`;
 
     if(!uri) {
         throw new Error('Could not connect to the database. No uri provided.');
     }
 
-    return mongoose.connect(uri, { user: process.env['DB_USER'], pass: process.env['DB_PW'] })
+    return mongoose.connect(uri, {
+      authSource: 'admin',
+      user: process.env['DB_USER'],
+      pass: process.env['DB_PW'],
+      dbName: process.env['DB_NAME']
+    })
         .then(() => console.log('Connected to the database'))
         .catch(err => console.log(`Could not connect to db. Err: ${err}`));
 }
