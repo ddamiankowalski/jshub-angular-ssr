94d7cbf0420a070aeaa0f7f56fb21e04a29814fd
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-05 21:19:59 +0100

    added menu navigation

--- apps/javascripthub/src/app/header/components/header-menu/header-menu.component.ts
+++ apps/javascripthub/src/app/header/components/header-menu/header-menu.component.ts
@@ -1,5 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
 import { ClassBinder } from "@javascripthub/utils";
+
+interface MenuItem {
+  label: string;
+  route: string;
+}
 
 @Component({
     standalone: true,
@@ -11,15 +17,21 @@ import { ClassBinder } from "@javascripthub/utils";
     providers: [ClassBinder]
 })
 export class HeaderMenuComponent {
-    public items = [
-        'Articles',
-        'Courses',
-        'Authors',
+    private _router = inject(Router)
+
+    public items: MenuItem[] = [
+        { label: 'Articles', route: '/' },
+        { label: 'Courses', route: '/' },
+        { label: 'Authors', route: 'authors' },
     ];
 
     private _classBinder = inject(ClassBinder);
 
     constructor() {
         this._classBinder.bind('jshub-header-menu');
+    }
+
+    public onItemClick(item: MenuItem): void {
+      this._router.navigate(['/', item.route])
     }
 }
