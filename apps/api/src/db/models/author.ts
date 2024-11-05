d2183432b1f6c46ac65ab30d6955eee7ebca5156
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-05 21:25:27 +0100

    added author schema

---
+++ apps/api/src/db/models/author.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const authorSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    description: { type: String, required: true },
+    twitterUrl: { type: String },
+    linkedInUrl: { type: String },
+    githubUrl: { type: String },
+}, { timestamps: true })
+
+export const Article = mongoose.model('Author', authorSchema);
