4a00f2380b4545fb9f05368e39cefb8a79f6c517
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-06 15:59:21 +0100

    added tests

---
+++ libs/articles/src/lib/components/article/article.component.spec.ts
@@ -0,0 +1,25 @@
+import { hasClass } from '@javascripthub/testing';
+import { ArticleComponent } from '../article/article.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute } from '@angular/router';
+
+describe('ArticleComponent', () => {
+  let fixture: ComponentFixture<ArticleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [{ provide: ActivatedRoute, useValue: {} }]
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(ArticleComponent);
+    fixture.detectChanges();
+  })
+
+  it('successfully initializes', () => {
+    expect(fixture).toBeTruthy();
+  })
+
+  it('has correct class', () => {
+    expect(hasClass(fixture, 'jshub-article')).toBeTruthy();
+  })
+})
