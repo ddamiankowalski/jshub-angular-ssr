b159ae63893abbbcc0bc4042273b1a5523534bbb
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-07 13:33:42 +0100

    added mobile header hamburger menu

--- apps/javascripthub/src/app/app.config.ts
+++ apps/javascripthub/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { appRoutes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
-import { cssComment, cssGitFork, cssHashtag, cssHeart, cssUserList } from '@ng-icons/css.gg';
+import { cssComment, cssGitFork, cssHashtag, cssHeart, cssMenu, cssUserList } from '@ng-icons/css.gg';
 import { bootstrapGithub, bootstrapLinkedin } from '@ng-icons/bootstrap-icons';
 
 export const appConfig: ApplicationConfig = {
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
-    provideIcons({ cssHeart, cssUserList, cssComment, cssHashtag, cssGitFork, bootstrapLinkedin, bootstrapGithub }),
+    provideIcons({ cssMenu, cssHeart, cssUserList, cssComment, cssHashtag, cssGitFork, bootstrapLinkedin, bootstrapGithub }),
     provideNgIconsConfig({
       size: '1rem',
     }),
--- apps/javascripthub/src/app/header/components/header-menu/header-menu.component.ts
+++ apps/javascripthub/src/app/header/components/header-menu/header-menu.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from "@angular/core";
 import { NavigationService } from "@javascripthub/navigation";
 import { ClassBinder } from "@javascripthub/utils";
+import { NgIcon } from "@ng-icons/core";
 
 interface MenuItem {
   label: string;
@@ -14,14 +15,15 @@ interface MenuItem {
     styleUrl: 'header-menu.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
-    providers: [ClassBinder]
+    providers: [ClassBinder],
+    imports: [NgIcon]
 })
 export class HeaderMenuComponent {
     private _navigation = inject(NavigationService)
 
     public items: MenuItem[] = [
         { label: 'Articles', route: '' },
-        { label: 'Courses', route: '' },
+        // { label: 'Courses', route: '' },
         { label: 'Authors', route: 'authors' },
     ];
 
