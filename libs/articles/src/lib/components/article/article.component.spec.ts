2263d1a91f065eeaa450b5ed6e90e461f8b8f5cc
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-07 23:08:22 +0100

    test fix

--- libs/articles/src/lib/components/article/article.component.spec.ts
+++ libs/articles/src/lib/components/article/article.component.spec.ts
@@ -2,13 +2,24 @@ import { hasClass } from '@javascripthub/testing';
 import { ArticleComponent } from '../article/article.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing'
 
 describe('ArticleComponent', () => {
   let fixture: ComponentFixture<ArticleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [{ provide: ActivatedRoute, useValue: {} }],
+      providers: [
+        { provide: ActivatedRoute, useValue: {
+            snapshot: { params: of('') },
+            params: of('')
+          }
+        },
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ArticleComponent);
