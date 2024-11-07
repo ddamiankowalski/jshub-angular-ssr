3b26611c09fcbf1c2fdb285618a0d997997a90ae
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-07 23:12:00 +0100

    test fix

--- apps/javascripthub/src/app/app.component.spec.ts
+++ apps/javascripthub/src/app/app.component.spec.ts
@@ -21,6 +21,6 @@ describe('AppComponent', () => {
   it(`should have as title 'javascripthub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('javascripthub');
+    expect(app.title).toEqual('jshub.dev');
   });
 });
